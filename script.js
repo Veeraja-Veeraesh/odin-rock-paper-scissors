26fe762a9b8c255d31f93219376d9cb973e0ca9e
WSL-Veeraja <veeraja.v@gmail.com>
2024-10-13 19:36:39 +0530

    Added js functionality for playing rock paper scissor

--- script.js
+++ script.js
@@ -18,26 +18,72 @@ function getHumanChoice() {
     return userChoice;
 }
 
-function playRound(compchoice, userchoice) {
-    if (compchoice === userchoice) {
-        console.log("It's a draw!");
+function declareWinners() {
+    if (humanScore === 5) {
+        alert(`Humans Win. Score: Humans = 5. Resetting Game...`);
+        humanScore = 0;
+        computerScore = 0;
+        results.textContent = `Scores: Computer = ${computerScore} Human = ${humanScore}`;
+        return;
     }
-    else if (compchoice === "rock" && userchoice === "scissor" || compchoice === "scissor" && userchoice === "paper" || compchoice === "paper" && userchoice === "rock") {
-        console.log(`Computer Wins! ${compchoice} beats ${userchoice}`);
-        console.log(`Scores: Computer = ${computerScore}\tHuman = ${humanScore}`);
-        computerScore++;
-    }
-    else {
-        console.log(`Human Wins! ${userchoice} beats ${compchoice}`);
-        console.log(`Scores: Computer = ${computerScore}\tHuman = ${humanScore}`);
-        humanScore++;
+    else if (computerScore === 5) {
+        alert(`Computers Win. Score: Computers = 5. Resetting Game...`);
+        humanScore = 0;
+        computerScore = 0;
+        results.textContent = `Scores: Computer = ${computerScore} Human = ${humanScore}`;
+        return;
     }
 }
 
-function playGame() {
-    for (i = 1; i <= 5; i++) {
-        compchoice = getComputerChoice();
-        humanchoice = getHumanChoice();
-        playRound(compchoice, humanchoice);
+function playRound() {
+    compchoice = getComputerChoice();
+    userchoice = this.textContent;
+    if (compchoice === userchoice) {
+        alert("It's a draw!");
+    }
+    else if (compchoice === "rock" && userchoice === "scissor" || compchoice === "scissor" && userchoice === "paper" || compchoice === "paper" && userchoice === "rock") {
+
+        alert(`Computer Wins! ${compchoice} beats ${userchoice}`);
+        computerScore++;
+        results.textContent = `Scores: Computer = ${computerScore} Human = ${humanScore}`;
+        if (computerScore === 5) declareWinners();
+    }
+    else {
+        alert(`Human Wins! ${userchoice} beats ${compchoice}`);
+        humanScore++;
+        results.textContent = `Scores: Computer = ${computerScore} Human = ${humanScore}`;
+        if (humanScore === 5) declareWinners();
     }
 }
+
+const container = document.createElement("div");
+container.classList.add("container");
+document.body.appendChild(container);
+
+const btn1 = document.createElement("button");
+btn1.textContent = "rock";
+btn1.classList.add("choice");
+
+const btn2 = document.createElement("button");
+btn2.textContent = "paper";
+btn2.classList.add("choice");
+
+const btn3 = document.createElement("button");
+btn3.textContent = "scissors";
+btn3.classList.add("choice");
+
+container.appendChild(btn1);
+container.appendChild(btn2);
+container.appendChild(btn3);
+
+
+const results = document.createElement("div");
+results.textContent = `Scores: Computer = ${computerScore} Human = ${humanScore}`;
+container.appendChild(results);
+
+const btns = document.querySelectorAll(".choice");
+
+btns.forEach(btn => {
+    btn.addEventListener("click", playRound);
+});
+
